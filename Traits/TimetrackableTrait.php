b2bda4e8cac866c65d970f9f6df093eda6b08cbd
vasilportey <portey@gmail.com>
2015-07-06 18:25:25 +0300

    init commit

---
+++ Traits/TimetrackableTrait.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mounter
+ * Date: 6/29/15
+ * Time: 11:43 AM
+ */
+
+namespace Youshido\DoctrineExtensionBundle\Traits;
+
+/**
+ * Class TimetrackableTrait
+ * @package Youshido\DoctrineExtensionBundle
+ */
+Trait TimetrackableTrait
+{
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Mapping\Annotation\Timestampable(on="create")
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * @Gedmo\Mapping\Annotation\Timestampable(on="update")
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+
+}
---
+++ YDoctrineExtensionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Youshido\DoctrineExtensionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YDoctrineExtensionBundle extends Bundle
+{
+}
